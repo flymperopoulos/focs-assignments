6e2aad56d80535a1a3720da1a2d2d120a089dc6c
flymperopoulos <filippos.lymperopoulos@students.olin.edu>
2016-09-11 21:50:54 -0400

    Turning in my assignment for day 3

--- day3/hw2.rkt
+++ day3/hw2.rkt
@@ -1,16 +1,34 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: Filippos Lymperopoulos
 ;;;
 ;;; Check one:
-;;; [ ] I completed this assignment without assistance or external resources.
+;;; [*] I completed this assignment without assistance or external resources.
 ;;; [ ] I completed this assignment with assistance from ___
 ;;;     and/or using these external resources: ___
 
 ;;; 1.  Create a calculator that takes one argument: a list that represents an expression.
 
 (define (calculate x)
-  your-code-here)
+  (if (or (number? x) (boolean? x))
+      x
+       (cond
+         [(eq? (first x) 'ADD) (+ (calculate (second x)) (calculate (third x)))]
+         [(eq? (first x) 'SUB) (- (calculate (second x)) (calculate (third x)))]
+         [(eq? (first x) 'MUL) (* (calculate (second x)) (calculate (third x)))]
+         [(eq? (first x) 'DIV) (/ (calculate (second x)) (calculate (third x)))]
+         [(eq? (first x) 'GT)  (> (calculate (second x)) (calculate (third x)))]
+         [(eq? (first x) 'LT)  (< (calculate (second x)) (calculate (third x)))]
+         [(eq? (first x) 'GE)  (>= (calculate (second x)) (calculate (third x)))]
+         [(eq? (first x) 'LE)  (<= (calculate (second x)) (calculate (third x)))]
+         [(eq? (first x) 'EQ)  (eq? (calculate (second x)) (calculate (third x)))]
+         [(eq? (first x) 'NEQ)  (not (eq? (calculate(second x)) (calculate(third x))))]
+         [(eq? (first x) 'ANND)  (and (calculate(second x)) (calculate(third x)))]
+         [(eq? (first x) 'ORR)  (or (calculate(second x)) (calculate(third x)))]
+         [(eq? (first x) 'NOTT)  (not (calculate(second x)) (calculate(third x)))]
+         [(eq? (first x) 'IPH) (if (calculate(second x)) (calculate (third x)) (calculate(fourth x)) )]
+         )
+  ))
 
 (calculate '(ADD 3 4)) ;; --> 7
 
@@ -23,13 +41,11 @@
 ;; Note that each of these takes numeric arguments (or expressions that evaluate to produce numeric values),
 ;; but returns a boolean.  We suggest operators `GT`, `LT`, `GE`, `LE`, `EQ`, `NEQ`.
 
-	(calculate '(GT (ADD 3 4) (MUL 5 6))) ;; --> #f
-	(calculate '(LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6))))) ;; --> #t
+(calculate '(GT (ADD 3 4) (MUL 5 6))) ;; --> #f
+(calculate '(LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6))))) ;; --> #t
 
 ;;; 4. Add boolean operations ANND, ORR, NOTT
-
-(calculate '(AND (GT (ADD 3 4) (MUL 5 6))) (LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6)))))) ;; --> #f
+(calculate '(ANND (GT (ADD 3 4) (MUL 5 6)) (LE (ADD 3 (MUL 4 5)) (SUB 0 (SUB (ADD 3 4) (MUL 5 6)))))) ;; --> #f
 
 ;;; 5. Add IPH
-
 (calculate '(IPH (GT (ADD 3 4) 7) (ADD 1 2) (ADD 1 3))) ;; -> 4
